ab8fc705070e49373b731c2551545a4aa144f361
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-05-08 16:19:57 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API/Controllers/ContactsController.cs
@@ -0,0 +1,31 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+       private readonly IContactService _contactService;
+        public ContactsController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+
+        [HttpGet("{id}")]
+
+        public ContactDTO GetContact(int id)
+        {
+            return _contactService.GetContactById(id);
+        }
+    
+    }
+}
---
+++ Services/ResAPI/ReservationAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+        public ReservationsController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+        [HttpGet("{id}")]
+        public ReservationDTO GetReservation(int id)
+        {
+            return _reservationService.GetResByBkgNumber(id);
+        }
+    }
+}
